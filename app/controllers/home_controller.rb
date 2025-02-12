848488bdf0bd89835c62bdc34a67f2aae14105c1
Roberto Garcia <robertogarcia280600@gmail.com>
2025-02-11 21:49:47 -0300

    add bootstrap and some improvements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,15 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @post = post
+  end
+
+  private
+
+  def post
+    [
+      { title: "Welcome to my blog", content: "This is a simple blog created with Ruby on Rails." },
+      { title: "My first post", content: "This is my first post. I hope you enjoy it." },
+      { title: "Another post", content: "This is another post. I hope you enjoy it." }
+    ]
+  end
 end
